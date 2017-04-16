ec5d9119d854d0552435d02080cd926cba06decf
lulian <lulian778899@gmail.com>
2017-04-17 06:43:52 +0800

    “我的文章的编辑删除功能”

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,60 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
 
-  def new
+  def edit
     @group = Group.find(params[:group_id])
-    @post = Post.new
+    @post = Post.find(params[:id])
+    @post.group = @group
   end
 
-  def create
+  def update
     @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
+    @post = Post.find(params[:id])
     @post.group = @group
     @post.user = current_user
 
-    if @post.save
-      redirect_to group_path(@group)
-    else
-      render :new
-    end
-  end
-
-
-  private
-
-  def post_params
-    params.require(:post).permit(:content)
-  end
-  
+    if @post.update(post_params)
+  redirect_to account_posts_path, notice: "編輯成功"
+else
+  render :edit
 end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to account_posts_path
+  end
+
+ def new
+   @group = Group.find(params[:group_id])
+   @post = Post.new
+
+   if !current_user.is_member_of?(@group)
+     redirect_to group_path(@group), alert: "要收藏才能评论喔!"
+   end
+ end
+
+ def create
+   @group = Group.find(params[:group_id])
+   @post = Post.new(post_params)
+   @post.group = @group
+   @post.user = current_user
+
+   if @post.save
+     redirect_to group_path(@group)
+   else
+     render :new
+   end
+ end
+
+
+ private
+
+ def post_params
+   params.require(:post).permit(:content)
+ end
+ end
